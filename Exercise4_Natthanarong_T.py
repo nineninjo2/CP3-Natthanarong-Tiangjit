b53ec79842fd63bc76f8a39ce485486657a2dcf4
nineninjo2 <50514192+nineninjo2@users.noreply.github.com>
2019-08-04 22:54:56 +0700

    Create Exercise4_Natthanarong_T.py

---
+++ Exercise4_Natthanarong_T.py
@@ -0,0 +1,9 @@
+fe = 27.00
+gb = 59.00
+ics = 99.00
+cp = 98.90
+print("--- Your Score ---")
+print("Foundation English : ",fe)
+print("General Business : ",gb)
+print("Introduction to Computer Systems : ",ics)
+print("Computer Programming : ",cp)
