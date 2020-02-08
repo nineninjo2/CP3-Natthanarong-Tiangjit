a422fe47a93f908fac8b1262f7eb129a3809dcd8
nineninjo2 <50514192+nineninjo2@users.noreply.github.com>
2020-02-08 17:00:38 +0700

    Create Lecture54_Natthanarong_T.py

---
+++ Lecture54_Natthanarong_T.py
@@ -0,0 +1,30 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "123":
+        return showMenu()
+    else:
+        print("user/password wrong")
+        return False
+def showMenu():
+    print("+++++++NineShop+++++++")
+    print("1.Vat Calculator")
+    print("2.Price Calculator")
+    return menuSelect()
+def menuSelect():
+    userselected = int(input("select 1 or 2 :"))
+    if userselected == 1:
+        return vatCalculate((int(input("totalprice:"))))
+    elif userselected == 2:
+        return priceCalculate()
+    else:
+        return showmenu()
+def vatCalculate(totalPrice):
+    result = totalPrice+(totalPrice*7/100)
+    print("Result", result)
+    return result
+def priceCalculate():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculate(price1+price2)
+login()
